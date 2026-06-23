91f983a72d4e1e83a34936e9d6e947410845a898
fchavonet <fchavonet@mail.fr>
2026-06-23 08:48:17 +0200

    feat: initialize frontend application

---
+++ frontend/src/api/client.js
@@ -0,0 +1,21 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+export async function apiRequest(path, options = {}) {
+  const url = API_URL + path;
+
+  const response = await fetch(url, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw data;
+  }
+
+  return data;
+}
