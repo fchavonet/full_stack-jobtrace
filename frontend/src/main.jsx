dcfafc066ee7255bb3faddd973ace9fe6e1f77eb
fchavonet <fchavonet@mail.fr>
2026-06-23 09:44:52 +0200

    feat: add authentication context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./global.css";
@@ -40,6 +42,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
